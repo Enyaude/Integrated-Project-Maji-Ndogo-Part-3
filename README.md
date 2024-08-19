6aa46ffa6a8fa155507b8cb9b81b7ab3a064a4d0
Samuel-Enyaude <enyaudesamuel@hotmail.com>
2024-08-19 13:38:05 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Integrated-Project-Maji-Ndogo-Part-3
+
+This phase involves consolidating data from multiple sources and employing statistical methods to assess the implications of a random audit. 
+The audit results confirm data accuracy and integrity. Addressing database errors is crucial to maintaining data reliability. 
---
+++ Setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Integrated-Project-Maji-Ndogo-Part-3',
+    version='0.3',
+    license='MIT',
+    description='EDSA example SQL package',
+    install_requires=['SQL', 'Mysql', 'JupyterNotebook'],
+    url='https://github.com/Enyaude/Integrated-Project-Maji-Ndogo-Part-3.git',
+    author='<Enyaude>',
+    author_email='<enyaudesamuel@hotmail.com>'
+    )
